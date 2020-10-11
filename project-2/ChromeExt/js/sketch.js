8675887efba88dc493549bc576e9f2dfbcc4c227
madelinef1127 <69998958+madelinef1127@users.noreply.github.com>
2020-10-11 02:04:48 -0700

    Update sketch.js

--- project-2/ChromeExt/js/sketch.js
+++ project-2/ChromeExt/js/sketch.js
@@ -26,10 +26,7 @@ function setup() {
 
 //start the falling of snacks and set their movements
 function draw() {
-  clear() //Make the background transparent
- 
-    
-  let var1 = random(1,5);
+  clear() //Make the background transparent 
 
   snacks.forEach(snack => {
     snack.collide();
@@ -93,7 +90,8 @@ class Snack {
     }
   }
   display() {
-    //if (var 1 , 2) {
+    let var1 = random(1,5);
+    if (var1 < 2) {
         //snack pattern 1 -- donut
         //dough
         noStroke();
@@ -130,15 +128,86 @@ class Snack {
         noFill();
         arc(this.x, this.y,88,88,225,45);
         arc(this.x, this.y,88,88,60,210);
-    //} 
-    //else if (2 <= i && i < 3) {
+    } 
+    else if (2 <= var1 && var1 < 3) {
+        //snack pattern 2 -- a bag of chips 
+        //bag
+        noStroke();
+        fill(230,57,0);
+        rect((this.x-40),(this.y-50),80,100);
+        arc((this.x+8),this.y,120,162,135,225);
+        arc((this.x-8),this.y,120,162,315,45);
+        fill(153,38,0);
+        rect((this.x-40),(this.y-55),80,5);
+        rect((this.x-40),(this.y+50),80,5);
+        fill(179,45,0);
+        triangle((this.x-40),(this.y-55),(this.x-30),(this.y-65),(this.x-20),(this.y-55));
+        triangle((this.x-20),(this.y-55),(this.x-10),(this.y-65),this.x,(this.y-55));
+        triangle(this.x,(this.y-55),(this.x+10),(this.y-65),(this.x+20),(this.y-55));
+        triangle((this.x+20),(this.y-55),(this.x+30),(this.y-65),(this.x+40),(this.y-55));
+        triangle((this.x-40),(this.y+55),(this.x-30),(this.y+65),(this.x-20),(this.y+55));
+        triangle((this.x-20),(this.y+55),(this.x-10),(this.y+65),this.x,(this.y+55));
+        triangle(this.x,(this.y+55),(this.x+10),(this.y+65),(this.x+20),(this.y+55));
+        triangle((this.x+20),(this.y+55),(this.x+30),(this.y+65),(this.x+40),(this.y+55));
+        //prints
+        fill(255,204,0);
+        ellipse(this.x,(this.y-10),50);
+        fill(255,255,255);
+        rect((this.x-30),(this.y-10),60,10);
+        rect((this.x-30),(this.y-20),60,4);
+        rect((this.x-38),(this.y+25),76,2);
+        fill(153,38,0);
+        rect((this.x-38),(this.y+30),76,8);
+        //highlight
+        stroke(255,121,77);
+        strokeWeight(4);
+        line((this.x-30),(this.y-45),(this.x+30),(this.y-45));
+        noFill();
+        arc((this.x-5),this.y,105,148,315,45);
+    }
+    else if (3 <= var1 && var1 < 4) {
+        //snack pattern 3 -- peppermint candy
+        //candy
+        noStroke();
+        fill(255,235,225);
+        ellipse(this.x,this.y,120);
+        fill(255,255,255);
+        ellipse(this.x,this.y,40);
+        //shadow and highlight
+        noFill();
+        stroke(255,200,160);
+        strokeWeight(6);
+        arc(this.x,this.y,110,110,45,225);
+        stroke(255,205,200);
+        arc(this.x,this.y,100,100,48,222);
+        stroke(255,255,255);
+        strokeWeight(6);
+        arc(this.x,this.y,110,110,250,20);
+        //spiral
+        translate(this.x,this.y);
+        fill(230,57,0); 
+        stroke(230,57,0); 
+        strokeWeight(1);
+        var r1=0,r2=0,step=1,spiralwidth=6,dw=spiralwidth/60;
+        beginShape(TRIANGLE_STRIP);
+        for ( var i = 0 ; i < 60 ; i++ ){
+          r1 += step;
+          spiralwidth -= dw;
+          r2 = r1 + spiralwidth;
+          var ang = PI/0.15;
+          var r1x = r1*sin(ang*i);
+          var r1y = r1*cos(ang*i);
+          var r2x = r2*sin(ang*i);
+          var r2y = r2*cos(ang*i);
+          vertex(r1x,r1y);
+          vertex(r2x,r2y);
+        }
+        endShape();
+        //coordinates back to original
+        translate(-this.x,-this.y);
+    }
+    else if (4 <= var1 && var1 < 5) }{
         
-    //}
-    //else if (3 <= i && i < 4) {
-        
-    //}
-    //else if (4 <= i && i < 5) }{
-        
-    //}
+    }
   }
 }
